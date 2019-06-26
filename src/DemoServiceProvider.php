b7a22d2b4d71464d0dba28bb3e6110633f6552dc
Đinh Quốc Hân <contact@dinhquochan.com>
2019-06-26 11:47:13 +0700

    Init commit

---
+++ src/DemoServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Blaze package.
+ *
+ * (c) Dinh Quoc Han <contact@dinhquochan.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Blaze\Demo;
+
+use Blaze\Support\ServiceProvider;
+
+class DemoServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
